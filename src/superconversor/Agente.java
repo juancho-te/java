716f098fb4b97efd234ff7aa35821fd0cfc79bb2
juancho-te <barriajuanalberto@gmail.com>
2022-09-03 17:42:12 -0300

    java conversor casi terminado

---
+++ src/superconversor/Agente.java
@@ -0,0 +1,71 @@
+package superconversor;
+import javax.swing.JOptionPane;
+import java.util.Arrays;
+
+
+
+public class Agente {
+	
+	
+	public static void main(String[] args) {
+		
+		Conversor conversor=new Conversor();   // instancio la clase y genero el objeto
+		String opcion;         // almacena la que se convierte---peso grados etc.... iimplementado solo moneda
+		String tipoDeMoneda;    // almacena tipo de moneda local....peso argento,shilenoo..etc
+		String tipoDeMonedaEx; // almacena moneda extrangera...dollar, euro...etcc
+		double cantidad;       // cantidad de dinero aconvertir
+		
+		String[] unidades={ "Moneda"};
+		String[] monedas={"Peso argentino"}; //, "pesos Chilenos","Soles","pesos colombianos","reales"};
+		String[] monedasEx=      {"Dolar","Euros","Libras","Yen japones", "Sul Coreano"};
+		double[] tablaDeValores= {135.54 , 897.60,    3.84, 4344.55     ,0.19};
+		boolean sigue=true;
+		
+		
+
+		conversor.setUnidadesConvertibles(unidades); //setea array de unidades
+		conversor.setMonedas(monedas); // setea array de monedas de latinoamerica;
+		conversor.setMonedasEx(monedasEx);
+		conversor.setTablaDeValores(tablaDeValores);
+		
+		
+		
+		while(sigue==true) {
+				int indiceMonedaEx=0; //almacena indice de moneda ex, segun la moneda extragera elegida
+				double calculo=0;     //almacena calculo en base a laoneda local y el valo de la moneda ex cuanto le corresponde
+								
+				//System.out.println(Arrays.asList(monedasEx).indexOf("Libras"));	
+				//que se convierte?
+				
+				opcion = (String)JOptionPane.showInputDialog(null, "¿Que desea convertir?", null, JOptionPane.QUESTION_MESSAGE, null, conversor.getUnidadesConvertibles(), conversor.getUnidadesConvertibles()[0]);
+				//System.out.println(opcion);
+		
+				//cuanta unidad?
+				conversor.setCantidadDeDinero(Double.parseDouble(JOptionPane.showInputDialog("¿Cuanto "+opcion+" desea convertir?")));
+				cantidad=conversor.getCantidadDeDinero();
+				//System.out.println(cantidad);
+		       	
+				// tipo de moneda local
+				tipoDeMoneda = (String)JOptionPane.showInputDialog(null, "¿Tipo de moneda?", null, JOptionPane.QUESTION_MESSAGE, null,monedas, monedas[0]);
+		       	//System.out.println(tipoDeMoneda);
+		       	
+		       	// a que tipo de moneda  extragera
+		       	tipoDeMonedaEx = (String)JOptionPane.showInputDialog(null, "¿A que tipo de moneda?", null, JOptionPane.QUESTION_MESSAGE, null,monedasEx, monedasEx[0]);
+		       	//System.out.println(tipoDeMonedaEx);
+		       	
+		       	indiceMonedaEx = Arrays.asList(monedasEx).indexOf(tipoDeMonedaEx);
+		       	
+		       	calculo=cantidad/tablaDeValores[indiceMonedaEx];
+		       	JOptionPane.showMessageDialog(null, "A la cantidad de "+cantidad+ " " + tipoDeMoneda +"le corresponde: "+String.format("%.2f", calculo)+" "+tipoDeMonedaEx+"s");
+		       	
+		       	int continua=JOptionPane.showConfirmDialog(null, "Desea consitnuar?");
+		       	//System.out.println(continua);
+		       	
+		       	if (continua==1) {
+		       		sigue=false;
+		       	}
+			}
+		System.exit(0);
+	}
+
+}
---
+++ src/superconversor/Conversor.java
@@ -0,0 +1,129 @@
+package superconversor;
+
+
+public class Conversor {
+	
+	private String[] unidadesConvertibles;
+	private String[] monedas;   //= {"Pesos argentinos", "pesos Chilenos","Soles","pesos colombianos","reales"};
+	private double[] tablaDeValores;    // =  {135.54,            897.60,            3.84,     4344.55,          0.19};	
+	private String[] monedasEx; //"Dolar","Euros","Libras","Yen japones", "Sul Coreano"};
+	private double conversion;
+
+		
+	public String[] getMonedasEx() {
+		return monedasEx;
+	}
+	public void setMonedasEx(String[] monedasEx) {
+		this.monedasEx = monedasEx;
+	}
+	public String[] getMonedas() {
+		return monedas;
+	}
+	public void setMonedas(String[] monedas) {
+		this.monedas = monedas;
+	}
+	
+	
+	
+	public double getConversion() {
+		return conversion;
+	}
+	public void setConversion(double conversion) {
+		this.conversion = conversion;
+	}
+	public String[] getUnidadesConvertibles() {
+		return unidadesConvertibles;
+	}
+	public void setUnidadesConvertibles(String[] unidadesConvertibles) {
+		this.unidadesConvertibles = unidadesConvertibles;
+	}
+	public String getTipoDemoneda() {
+		return tipoDemoneda;
+	}
+	public void setTipoDemoneda(String tipoDemoneda) {
+		this.tipoDemoneda = tipoDemoneda;
+	}
+	public double getCantidadDeDinero() {
+		return cantidadDeDinero;
+	}
+	public void setCantidadDeDinero(double cantidadDeDinero) {
+		this.cantidadDeDinero = cantidadDeDinero;
+	}
+	private String tipoDemoneda;
+	private double cantidadDeDinero;
+
+	public double[] getTablaDeValores() {
+		return tablaDeValores;
+	}
+	public void setTablaDeValores(double[] tablaDeValores) {
+		this.tablaDeValores = tablaDeValores;
+	}
+
+
+
+	
+	
+	
+	
+	
+	
+		
+	/*
+	public static void main(String[] args) {
+			
+			String[] options = {"Temperatura", "Moneda", "Velocidad Maritima", "Metros"};
+			String[] monedas = {"Pesos argentinos", "pesos Chilenos","Soles","pesos colombianos","reales"};
+			Double[] factor =  {135.54,            897.60,            3.84,     4344.55,          0.19};
+			
+			double conversion;
+			String opcion;
+			String tipoDemoneda;
+			double cantidadDeDinero;
+			
+
+	        opcion = (String)JOptionPane.showInputDialog(null, "¿Que desea convertir?", null, JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+	        
+	        System.out.println(opcion);
+	        cantidadDeDinero= Double.parseDouble(JOptionPane.showInputDialog("¿Cuanto "+opcion+" desea convertir?"));
+	        
+	        System.out.println(cantidadDeDinero);
+
+	       	tipoDemoneda = (String)JOptionPane.showInputDialog(null, "¿Tipo de moneda?", null, JOptionPane.QUESTION_MESSAGE, null,monedas, monedas[0]);
+	        
+	       	System.out.println(tipoDemoneda);
+	       	
+	       	
+	       	switch(opcion) {
+	       		case "Moneda":
+	       			System.out.println("eligio conversion de monedas");
+	       			break;
+	       			
+	       		case "Temperatura":
+	       			System.out.println("Eleigio temperatura");
+	       			break;
+	       		
+	       		default:
+	       			break;
+	       	
+	       	
+	       	
+	       	}
+	       	
+	       	
+	       	
+	       	
+	       	
+	       	
+		} */
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+	}
+
